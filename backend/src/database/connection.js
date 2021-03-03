14d6c28c1269c9cad73a045c62cc28b5807ffd64
Pedro Henrique <henriquepedro8581@gmail.com>
2021-03-03 19:33:36 -0300

    Back-end da aplicação

---
+++ backend/src/database/connection.js
@@ -0,0 +1,7 @@
+const Knex = require('knex');
+const knex = require('knex');
+const configuration = require('../../knexfile');
+
+const connection = knex(configuration.development);
+
+module.exports = connection;
